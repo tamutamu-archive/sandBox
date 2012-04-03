456fbee1f1acfa3bb6166ff0646500f38be1ebe8
tamutamu <tamu.0.0.tamu@gmail.com>
2012-04-03 10:34:29 +0900

    test

--- com.homeserver.tamutamu.AutoSuiteGen/src/com/homeserver/tamutamu/autosuitegen/AutoSuiteGenPropertyPage.java
+++ com.homeserver.tamutamu.AutoSuiteGen/src/com/homeserver/tamutamu/autosuitegen/AutoSuiteGenPropertyPage.java
@@ -3,7 +3,6 @@ package com.homeserver.tamutamu.autosuitegen;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.QualifiedName;
-import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.search.SearchEngine;
@@ -11,6 +10,7 @@ import org.eclipse.jdt.ui.IJavaElementSearchConstants;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -32,11 +32,10 @@ public class AutoSuiteGenPropertyPage extends PropertyPage implements
 
 	private Text testClassName;
 	private List superTestCaseList = null;
-	private Button buttonAdd = null;
-	private Button buttonRemove = null;
+	private List runnerList = null;
 
 	public AutoSuiteGenPropertyPage() {
-		setMessage("Suite自動生成の設定です。");
+		setMessage("Suite自動生成");
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
 	}
 
@@ -56,97 +55,20 @@ public class AutoSuiteGenPropertyPage extends PropertyPage implements
 		// 1. テストクラス名パターン
 		Group gp1 = new Group(mainComp, SWT.NONE);
 		gp1.setLayout(new GridLayout(1, true));
-		gp1.setText("１．テストクラス名パターン");
+		gp1.setText("1. テストクラス名パターン（Junit3/4共通）");
 		gp1.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 
 		testClassName = new Text(gp1, SWT.SINGLE | SWT.BORDER);
 		testClassName.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		testClassName.setToolTipText("テストクラス名のパターンを正規表現で指定してください。");
+		testClassName.setToolTipText("テストクラス名のパターンを正規表現で指定してください。\nこの条件に合致しない場合、無条件にテスト非対象です。");
 
 		// 2. 親テストクラスによる絞込み
-		Group gp2 = new Group(mainComp, SWT.NONE);
-		gp2.setLayout(new GridLayout(3, true));
-		gp2.setText("２．親テストクラスによる絞込み");
-		GridData gp2_gd = new GridData(GridData.FILL_HORIZONTAL);
-		gp2_gd.horizontalSpan = 3;
-		gp2.setLayoutData(gp2_gd);
+		superTestCaseList = createClassSelectList(mainComp, "2. さらに親クラスで絞り込み（JUnit3用）", "いずれかにマッチすればテスト対象");
 
-		GridData superTestCaseList_gd = new GridData(GridData.FILL_HORIZONTAL);
-		superTestCaseList_gd.horizontalSpan = 2;
-		superTestCaseList_gd.heightHint = 80;
-		superTestCaseList = new List(gp2, SWT.H_SCROLL | SWT.V_SCROLL
-				| SWT.BORDER);
-		// superTestCaseList.setSize(2000, 200);
-		superTestCaseList.setLayoutData(superTestCaseList_gd);
+		// 3. @RunWithのRunnerによる絞込み
+		runnerList = createClassSelectList(mainComp, "3. さらに@RunWithのテストランナーで絞り込み（JUnit4用）", "いずれかにマッチすればテスト対象");
 
 		this.doLoad();
-
-		Composite buttonComp = new Composite(gp2, SWT.NONE);
-		buttonComp.setLayout(new GridLayout(1, false));
-		buttonComp.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-		buttonAdd = new Button(buttonComp, SWT.NONE);
-		buttonAdd.setText("追加");
-		buttonAdd.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		buttonAdd
-				.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
-					public void widgetSelected(
-							org.eclipse.swt.events.SelectionEvent e) {
-						// final SelectSuperTestCaseDlg dlg = new
-						// SelectSuperTestCaseDlg(
-						// new Shell(), "親テストクラスの選択");
-						try {
-							SelectionDialog dialog;
-
-							dialog = JavaUI
-									.createTypeDialog(
-											new Shell(),
-											Activator.getDefault()
-													.getWorkbench()
-													.getActiveWorkbenchWindow(),
-											SearchEngine.createWorkspaceScope(),
-											IJavaElementSearchConstants.CONSIDER_CLASSES,
-											false);
-							dialog.setTitle("絞り込み対象の親クラスを選択");
-							if (dialog.open() == Dialog.OK) {
-								String superTestCaseClassName = ((IType) dialog
-										.getResult()[0])
-										.getFullyQualifiedName();
-
-								superTestCaseList.setData(
-										superTestCaseClassName,
-										superTestCaseClassName);
-								superTestCaseList.add(superTestCaseClassName);
-								superTestCaseList.select(superTestCaseList
-										.getItemCount() - 1);
-							}
-
-						} catch (JavaModelException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-					}
-				});
-
-		buttonRemove = new Button(buttonComp, SWT.NONE);
-		buttonRemove.setText("削除");
-		buttonRemove.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		buttonRemove
-				.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
-					public void widgetSelected(
-							org.eclipse.swt.events.SelectionEvent e) {
-						int idx = superTestCaseList.getSelectionIndex();
-						if (idx < 0)
-							return;
-						superTestCaseList.remove(idx);
-						if (superTestCaseList.getItemCount() > 0) {
-							if (idx == 0)
-								idx += 1;
-							superTestCaseList.select(idx - 1);
-
-						}
-					}
-				});
 
 		return mainComp;
 	}
@@ -162,15 +84,21 @@ public class AutoSuiteGenPropertyPage extends PropertyPage implements
 		super.performDefaults();
 	}
 
-	public void doLoad() {
+	/**
+	 * 設定をロードします。
+	 */
+	private void doLoad() {
 
-		IProject project =( (IJavaProject) getElement()).getProject();
+		IProject project = (IProject) getElement().getAdapter(IProject.class);
 
-		String testCaseNamePTN = ProjectPropertyUtil.getTestCaseNamePTN(project);
-		if(testCaseNamePTN == null) testCaseNamePTN = "";
+		String testCaseNamePTN = ProjectPropertyUtil
+				.getTestCaseNamePTN(project);
+		if (testCaseNamePTN == null)
+			testCaseNamePTN = "";
 		testClassName.setText(testCaseNamePTN);
 
-		String[] parentTestCase = ProjectPropertyUtil.getParentTestCaseList(project);
+		String[] parentTestCase = ProjectPropertyUtil.getParentTestCaseList(
+				project, ProjectPropertyUtil.SUPER_TESTCLASS_LIST_KEY);
 
 		for (int i = 0; i < parentTestCase.length; i++) {
 			superTestCaseList.setData(parentTestCase[i], parentTestCase[i]);
@@ -180,17 +108,35 @@ public class AutoSuiteGenPropertyPage extends PropertyPage implements
 		if (superTestCaseList.getItemCount() > 0) {
 			superTestCaseList.select(0);
 		}
+
+		String[] testRunner = ProjectPropertyUtil.getParentTestCaseList(
+				project, ProjectPropertyUtil.TESTRUNNER_LIST_KEY);
+
+		for (int i = 0; i < testRunner.length; i++) {
+			runnerList.setData(testRunner[i], testRunner[i]);
+			runnerList.add(testRunner[i]);
+		}
+
+		if (runnerList.getItemCount() > 0) {
+			runnerList.select(0);
+		}
 	}
 
-	public void doStore() {
+	/**
+	 * 設定を保存します。
+	 */
+	private void doStore() {
 
-		IProject project =( (IJavaProject) getElement()).getProject();
+		IProject project = (IProject) getElement().getAdapter(IProject.class);
+
 		try {
-			project.setPersistentProperty(
-					new QualifiedName(Activator.PLUGIN_ID,
-							ProjectPropertyUtil.TEST_CLASSNAME_PTN_KEY),
-					testClassName.getText());
+			// テストクラス名のパターン
+			project.setPersistentProperty(new QualifiedName(
+					Activator.PLUGIN_ID,
+					ProjectPropertyUtil.TEST_CLASSNAME_PTN_KEY), testClassName
+					.getText());
 
+			// 対象親クラス
 			String parentTestCase = "";
 			for (int i = 0; i < superTestCaseList.getItemCount(); i++) {
 				parentTestCase += (superTestCaseList.getItem(i) + ";");
@@ -198,11 +144,116 @@ public class AutoSuiteGenPropertyPage extends PropertyPage implements
 
 			project.setPersistentProperty(new QualifiedName(
 					Activator.PLUGIN_ID,
-					ProjectPropertyUtil.SUPER_TESTCLASS_LIST_KEY), parentTestCase);
+					ProjectPropertyUtil.SUPER_TESTCLASS_LIST_KEY),
+					parentTestCase);
+
+			// 対象@RunWithのテストランナー
+			String testRunner = "";
+			for (int i = 0; i < runnerList.getItemCount(); i++) {
+				testRunner += (runnerList.getItem(i) + ";");
+			}
+
+			project.setPersistentProperty(new QualifiedName(
+					Activator.PLUGIN_ID,
+					ProjectPropertyUtil.TESTRUNNER_LIST_KEY), testRunner);
+
 		} catch (CoreException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	/**
+	 * クラス選択リストを作成します。
+	 * 
+	 * @param mainComp
+	 * @param title
+	 * @return
+	 */
+	private List createClassSelectList(Composite mainComp, String title, String toolTip) {
+
+		Group gp = new Group(mainComp, SWT.NONE);
+		gp.setLayout(new GridLayout(3, true));
+		gp.setText(title);
+		GridData gp_gd = new GridData(GridData.FILL_HORIZONTAL);
+		gp_gd.horizontalSpan = 3;
+		gp.setLayoutData(gp_gd);
+
+		GridData caseList_gd = new GridData(GridData.FILL_HORIZONTAL);
+		caseList_gd.horizontalSpan = 2;
+		caseList_gd.heightHint = 80;
+		final List classList = new List(gp, SWT.H_SCROLL | SWT.V_SCROLL
+				| SWT.BORDER);
+		classList.setLayoutData(caseList_gd);
+
+		Composite buttonComp = new Composite(gp, SWT.NONE);
+		buttonComp.setLayout(new GridLayout(1, false));
+		buttonComp.setLayoutData(new GridData(GridData.FILL_BOTH));
+
+		Button buttonAdd = new Button(buttonComp, SWT.NONE);
+		buttonAdd.setText("追加");
+		buttonAdd.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		buttonAdd
+				.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
+					public void widgetSelected(
+							org.eclipse.swt.events.SelectionEvent e) {
+						try {
+							SelectionDialog dialog;
+
+							dialog = JavaUI
+									.createTypeDialog(
+											new Shell(),
+											Activator.getDefault()
+													.getWorkbench()
+													.getActiveWorkbenchWindow(),
+											SearchEngine.createWorkspaceScope(),
+											IJavaElementSearchConstants.CONSIDER_CLASSES,
+											false);
+							dialog.setTitle("絞り込み対象の親クラスを選択");
+							if (dialog.open() == Dialog.OK) {
+								String selectClassName = ((IType) dialog
+										.getResult()[0])
+										.getFullyQualifiedName();
+								
+								for(String className : classList.getItems()){
+									if(className.equals(selectClassName)) return;
+								}
+
+								classList.setData(selectClassName,
+										selectClassName);
+								classList.add(selectClassName);
+								classList.select(classList.getItemCount() - 1);
+							}
+
+						} catch (JavaModelException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
+
+		Button buttonRemove = new Button(buttonComp, SWT.NONE);
+		buttonRemove.setText("削除");
+		buttonRemove.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		buttonRemove
+				.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
+					public void widgetSelected(
+							org.eclipse.swt.events.SelectionEvent e) {
+						int idx = classList.getSelectionIndex();
+						if (idx < 0)
+							return;
+						classList.remove(idx);
+						if (classList.getItemCount() > 0) {
+							if (idx == 0)
+								idx += 1;
+							classList.select(idx - 1);
+
+						}
+					}
+				});
+		
+		classList.setToolTipText(toolTip);
+		
+		return classList;
+
 	}
 
 }
